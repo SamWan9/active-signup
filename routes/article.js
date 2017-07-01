cc892d33b83ea2ac5ae4375f39450dfb0d638374
sam wang <976244734@qq.com>
2017-07-01 16:01:28 +0800

    ejs模板引擎配置2

--- routes/article.js
+++ routes/article.js
@@ -2,11 +2,11 @@ const express=require("express");
 const router=express.Router();
 
 router.get("/add",(req,res)=>{
-    res.send("增加文章");
+    res.render("user/signup",{title: "注册",content: "用户注册"});
 });
 
 router.get("/delete/:id",(req,res)=>{
-    res.send("删除文章");
+    res.render("user/signup",{title: "注册",content: "用户注册"});
 });
 
 module.exports=router;
