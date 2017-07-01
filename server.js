0c37f45a1cd97593fffe472c91b3c187f65fa09f
sam wang <976244734@qq.com>
2017-07-01 15:57:52 +0800

    ejs模板引擎配置

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express=require("express");
+const path=require("path");
 const app=express();
 const user=require("./routes/user");
 const category=require("./routes/category");
 const article=require("./routes/article");
+
+app.set("view engine","html");
+app.set("views",path.resolve("views"));
+app.engine("html",require("ejs").__express);
 
 app.use("/user",user);
 app.use("/category",category);
